161467b3283cfcf723d30d552b3b3a25b74bedb5
aprameyaneopane <aprameyanwopane@gmail.com>
2021-07-26 16:17:15 +0545

    Changes

--- ChangeColor/main.js
+++ ChangeColor/main.js
@@ -30,7 +30,7 @@ function colorChanger() {
     }
 
     orange.onclick = function () {
-        element.style.color = "rgb(255, 102, 0)";
+        element.style.color = "rgb(255, 102, 0.1)";
     }
 
 }
