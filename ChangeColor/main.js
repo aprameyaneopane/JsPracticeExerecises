6f2ac3708384d910f8983326b3270b10bdbe96b3
aprameyaneopane <aprameyanwopane@gmail.com>
2021-07-26 12:02:10 +0545

    first commit

---
+++ ChangeColor/main.js
@@ -0,0 +1,38 @@
+function colorChanger() {
+
+    let element = document.getElementById("changeColor");
+
+    let blue = document.getElementById("blue");
+    let red = document.getElementById("red");
+    let yellow = document.getElementById("yellow");
+    let green = document.getElementById("green");
+    let purple = document.getElementById("purple");
+    let orange = document.getElementById("orange");
+
+    // element.style.color = "none";  
+
+    blue.onclick = function () {
+        element.style.color = "blue";
+    }
+
+    red.onclick = function () {
+        element.style.color = "red";
+    }
+
+    yellow.onclick = function () {
+        element.style.color = "gold";
+    }
+
+    green.onclick = function () {
+        element.style.color = "green";
+    }
+
+    purple.onclick = function () {
+        element.style.color = "purple";
+    }
+
+    orange.onclick = function () {
+        element.style.color = "orange";
+    }
+
+}
