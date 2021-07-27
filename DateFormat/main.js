51a9c2f09514be0661464c231cc06e7aae9b55cc
aprameyaneopane <aprameyanwopane@gmail.com>
2021-07-27 11:56:10 +0545

    changes

--- DateFormat/main.js
+++ DateFormat/main.js
@@ -9,7 +9,34 @@ let formatOne = document.getElementById("one");
 let formatTwo = document.getElementById("two");
 let formatThree = document.getElementById("three");
 
+if ((month < 10) && (day < 10)) {
+    let MMDDYYYY = `Today is 0${month} / 0${day} / ${year}`;
 
-let MMDDYYYY = `Today is ${month} / ${day} / ${year}`;
+    let DDMMYYYY = `Today is 0${day} / 0${month} / ${year}`;
 
-formatOne.innerHTML = MMDDYYYY;
+    let YYYYMMDD = `Today is ${year} / 0${month} / 0${day}`;
+
+    formatOne.innerHTML = MMDDYYYY;
+
+    formatTwo.innerHTML = DDMMYYYY;
+
+    formatThree.innerHTML = YYYYMMDD;
+}
+
+else if ((month < 10) && (day > 10)) {
+    let MMDDYYYY = `Today is 0${month} / ${day} / ${year}`;
+
+    let DDMMYYYY = `Today is ${day} / 0${month} / ${year}`;
+
+    let YYYYMMDD = `Today is ${year} / 0${month} / ${day}`;
+
+    formatOne.innerHTML = MMDDYYYY;
+
+    formatTwo.innerHTML = DDMMYYYY;
+
+    formatThree.innerHTML = YYYYMMDD;
+}
+
+
+
+
